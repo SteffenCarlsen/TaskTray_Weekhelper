312d8b3da5cfdd05a5eea84bee04c19474a217e5
Steffen Carlsen <stca@netcompany.com>
2024-03-05 16:20:33 +0100

    Added date -> weeknumber command and cleand up code a bit

--- TaskTrayWeek/WeekApplicationContext.cs
+++ TaskTrayWeek/WeekApplicationContext.cs
@@ -7,59 +7,107 @@ public class WeekApplicationContext : ApplicationContext
 {
     private const int FONTSIZE_SMALL = 15;
     private const int FONTSIZE_BIG = 21;
-    
-    private readonly NotifyIcon _trayIcon;
-    private readonly ToolStripTextBox _getWeekNUm; 
-    public WeekApplicationContext ()
+    private ToolStripMenuItem _exitApplicationItem;
+    private ToolStripTextBox _getWeekNumFromDateItem;
+    private ToolStripTextBox _getWeekNumItem;
+    private ToolStripMenuItem _manuallyUpdateIconItem;
+    private ToolStripMenuItem _toolTipTitleItem;
+
+    private NotifyIcon _trayIcon;
+
+    public WeekApplicationContext()
     {
-        // Check if week has changed once every hour incase some pesky person forgot to shut their PC down
+        InitializeTimer();
+        InitializeMenuItem();
+        InitializeToolStripTextBoxes();
+        InitializeTrayIcon();
+        InitializeApplication();
+    }
+
+    private void InitializeTimer()
+    {
         var timer = new Timer
         {
-            Interval = 3600000,
+            Interval = 3600000
         };
         timer.Elapsed += TimerOnElapsed;
-        
-        var itemTop = new ToolStripMenuItem("Current week")
+        timer.Start();
+    }
+
+    private void InitializeMenuItem()
+    {
+        _toolTipTitleItem = new ToolStripMenuItem("Current week")
         {
             Enabled = false
         };
+        _manuallyUpdateIconItem = new ToolStripMenuItem("Force update", null, (_, _) => TimerOnElapsed(this, EventArgs.Empty));
+        _exitApplicationItem = new ToolStripMenuItem("Exit", null, OnApplicationExit);
+    }
 
-        var itemUpdate = new ToolStripMenuItem("Force update", null, (_, _) => TimerOnElapsed(this, EventArgs.Empty));
-        
-        _getWeekNUm = new ToolStripTextBox("Get date from week");
-        _getWeekNUm.TextBox!.PlaceholderText = "Enter week number and press enter";
-        _getWeekNUm.KeyDown += GetWeekNUmOnKeyDown;
-        
-        var item = new ToolStripMenuItem("Exit", null, OnApplicationExit);
-        
-        // Initialize Tray Icon
-        _trayIcon = new NotifyIcon()
+    private void InitializeToolStripTextBoxes()
+    {
+        _getWeekNumItem = new ToolStripTextBox("Get date from week");
+        _getWeekNumItem.TextBox!.PlaceholderText = "Enter weeknumber";
+        _getWeekNumItem.KeyDown += GetWeekNumItemOnKeyDown;
+
+        _getWeekNumFromDateItem = new ToolStripTextBox("Get week from date");
+        _getWeekNumFromDateItem.TextBox!.PlaceholderText = "Enter date";
+        _getWeekNumFromDateItem.KeyDown += GetWeekNumFromDateItemOnKeyDown;
+    }
+
+    private void InitializeTrayIcon()
+    {
+        _trayIcon = new NotifyIcon
         {
             Visible = true,
             Icon = GenerateIcon(0),
             BalloonTipText = "Current week",
             Text = "Current week",
-            ContextMenuStrip = new ContextMenuStrip() { Items = { itemTop, itemUpdate, _getWeekNUm, item }}
+            ContextMenuStrip = new ContextMenuStrip
+            {
+                Items =
+                {
+                    _toolTipTitleItem, _manuallyUpdateIconItem, _getWeekNumItem, _getWeekNumFromDateItem,
+                    _exitApplicationItem
+                }
+            }
         };
-
-        // Initialize application
-        TimerOnElapsed(this,EventArgs.Empty);
-        Application.ApplicationExit += OnApplicationExit;
-        timer.Start();
     }
 
-    private void GetWeekNUmOnKeyDown(object? sender, KeyEventArgs e)
+    private void InitializeApplication()
     {
-        if (e.KeyCode == Keys.Enter && _getWeekNUm.TextBox != null)
+        TimerOnElapsed(this, EventArgs.Empty);
+        Application.ApplicationExit += OnApplicationExit;
+    }
+
+    private void GetWeekNumItemOnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.KeyCode == Keys.Enter && _getWeekNumItem.TextBox != null)
         {
-            if (int.TryParse(_getWeekNUm.TextBox.Text, out var result))
+            if (int.TryParse(_getWeekNumItem.TextBox.Text, out var result))
             {
                 var weekNum = FirstDateOfWeekISO8601(result);
-                _getWeekNUm.TextBox.Text = weekNum.ToShortDateString();
+                _getWeekNumItem.TextBox.Text = weekNum.ToShortDateString();
             }
             else
             {
-                _getWeekNUm.TextBox.Text = "Badly formatted string";
+                _getWeekNumItem.TextBox.Text = "Badly formatted string";
+            }
+        }
+    }
+
+    private void GetWeekNumFromDateItemOnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.KeyCode == Keys.Enter && _getWeekNumFromDateItem.TextBox != null)
+        {
+            if (DateOnly.TryParse(_getWeekNumFromDateItem.TextBox.Text, out var result))
+            {
+                var weekNum = GetWeekNumber(result);
+                _getWeekNumFromDateItem.TextBox.Text = weekNum.ToString();
+            }
+            else
+            {
+                _getWeekNumFromDateItem.TextBox.Text = "Badly formatted string";
             }
         }
     }
@@ -78,7 +126,7 @@ public class WeekApplicationContext : ApplicationContext
     }
 
     /// <summary>
-    /// Dynamically generate a weeknumber icon for the application
+    ///     Dynamically generate a weeknumber icon for the application
     /// </summary>
     /// <param name="weekNum">Weeknumber to generate icon for</param>
     /// <returns>A windows application icon</returns>
@@ -87,11 +135,12 @@ public class WeekApplicationContext : ApplicationContext
         var bitmap = new Bitmap(32, 32);
         using (var g = Graphics.FromImage(bitmap))
         {
-            g.FillRectangle(new SolidBrush(Color.Black), new Rectangle(0,0,32,32));
-            g.DrawString(weekNum.ToString(), new Font("Verdana", weekNum > 9 ? FONTSIZE_SMALL : FONTSIZE_BIG, FontStyle.Bold), new SolidBrush(Color.White), new PointF(0, 2));
+            g.FillRectangle(new SolidBrush(Color.Black), new Rectangle(0, 0, 32, 32));
+            g.DrawString(weekNum.ToString(),
+                new Font("Verdana", weekNum > 9 ? FONTSIZE_SMALL : FONTSIZE_BIG, FontStyle.Bold),
+                new SolidBrush(Color.White), new PointF(0, 2));
             return Icon.FromHandle(bitmap.GetHicon());
         }
-        
     }
 
     private int GetWeekNumber(DateTime date)
@@ -99,9 +148,14 @@ public class WeekApplicationContext : ApplicationContext
         var ciCurr = CultureInfo.CurrentCulture;
         return ciCurr.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
     }
-    
+
+    private int GetWeekNumber(DateOnly date)
+    {
+        return GetWeekNumber(date.ToDateTime(TimeOnly.MinValue));
+    }
+
     /// <summary>
-    /// Gets the date of the Monday in a week depending on the weeknumber following the ISO8601 standard
+    ///     Gets the date of the Monday in a week depending on the weeknumber following the ISO8601 standard
     /// </summary>
     /// <param name="weekOfYear">Week number to get the date from</param>
     /// <returns></returns>
@@ -119,10 +173,7 @@ public class WeekApplicationContext : ApplicationContext
         var weekNum = weekOfYear;
         // As we're adding days to a date in Week 1,
         // we need to subtract 1 in order to get the right date for week #1
-        if (firstWeek == 1)
-        {
-            weekNum -= 1;
-        }
+        if (firstWeek == 1) weekNum -= 1;
 
         // Using the first Thursday as starting week ensures that we are starting in the right year
         // then we add number of weeks multiplied with days
@@ -130,6 +181,5 @@ public class WeekApplicationContext : ApplicationContext
 
         // Subtract 3 days from Thursday to get Monday, which is the first weekday in ISO8601
         return result.AddDays(-3);
-    }    
-    
+    }
 }
